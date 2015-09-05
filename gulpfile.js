c2893a08ee051fdd5ecfb7288007c2e4cd50a666
Davide Cantoni <dev@davidecantoni.ch>
2015-09-05 13:00:57 +0400

    Add gulpfile incl tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,54 @@
-var gulp = require('gulp'),
+var gulp    = require('gulp'),
     connect = require('gulp-connect'),
-    babel = require('gulp-babel');
+    babel   = require('gulp-babel'),
+    uglify  = require('gulp-uglify'),
+    sass    = require('gulp-sass'),
+    jshint  = require('gulp-jshint'),
+    rename  = require('gulp-rename'),
+    minify  = require('gulp-minify-css');
 
 gulp.task('connect', function() {
     connect.server({
-        root: './',
+        root: [
+            'helper',
+            'dist',
+            'node_modules'
+        ],
         livereload: true
     });
 });
 
-gulp.task('babel', function () {
-    return gulp.src('src/*.js')
+gulp.task('sass', function () {
+    gulp.src('./src/scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(minify({compatibility: 'ie8'}))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('js', function () {
+    return gulp.src('./src/js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default', {
+            verbose: true
+        }))
         .pipe(babel())
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist/js'))
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('html', function () {
-    gulp.src('./*.html')
+    gulp.src('./helper/*.html')
         .pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
-    gulp.watch(['./*.html'], ['html']);
-    gulp.watch(['./src/*.js'], ['babel']);
+    gulp.watch(['./helper/*.html'], ['html']);
+    gulp.watch(['./src/scss/*.scss'], ['sass']);
+    gulp.watch(['./src/js/*.js'], ['js']);
 });
 
-gulp.task('default', ['connect', 'watch']);
+gulp.task('default', ['sass', 'js', 'connect', 'watch']);
